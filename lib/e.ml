171cb86bb003cd6487c68c6e58ad7f52ad0c5cf8
Dmitry Ponyatov <dponyatov@gmail.com>
2024-12-27 20:59:00 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/e.ml
+++ lib/e.ml
@@ -1,3 +1,10 @@
 let app = "eml"
 let author = "Dmitry Ponyatov"
 let email = "dponyatov@gmail.com"
+
+(* *)
+let mkd d = if not (Sys.is_directory d) then Sys.mkdir d 0x750
+
+let dirs =
+  mkd app;
+  mkd (app ^ "/" ^ ".vscode")
