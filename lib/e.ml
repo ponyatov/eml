48cfd804dd77c6f85bf3ac6ded85ca0b6ec01952
Dmitry Ponyatov <dponyatov@gmail.com>
2024-12-27 21:26:19 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/e.ml
+++ lib/e.ml
@@ -3,8 +3,24 @@ let author = "Dmitry Ponyatov"
 let email = "dponyatov@gmail.com"
 
 (* *)
-let mkd d = if not (Sys.is_directory d) then Sys.mkdir d 0x750
+let mkd d = try (Sys.mkdir d 0o740) with Sys_error _ -> ()
+
+let vscode =
+  let vsc = app ^ "/.vscode" in
+  mkd vsc;
+  let ext = open_out (vsc ^ "/extensions.json") in
+  close_out ext
+
+  let bin = mkd (app^"/bin")
+  let doc = mkd (app^"/doc")
+  let lib = mkd (app^"/lib")
+  let inc = mkd (app^"/inc")
+  let src = mkd (app^"/src")
+  let tmp = mkd (app^"/tmp")
+   
 
 let dirs =
   mkd app;
-  mkd (app ^ "/" ^ ".vscode")
+  vscode; bin; doc; lib; inc; src; tmp;
+
+let () = dirs
