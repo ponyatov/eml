5a67e67754c6caac5eb4062dc1cc8072b4e24b40
Dmitry Ponyatov <dponyatov@gmail.com>
2024-12-29 12:33:01 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/expr.ml
+++ lib/expr.ml
@@ -12,3 +12,17 @@ let x = Int (-1)
 let y = Int 2
 let sum = BinOp (Add, x, y)
 let neg = Pfx (Sub, sum)
+
+
+let rec eval expr  =
+  match expr with
+  | Int i -> i
+  | BinOp (Add,x,y) -> (eval x) + (eval y)
+  | Pfx (Sub,e) -> -(eval e)
+  | _ -> failwith "not implemented"
+
+assert (eval x == -1)
+assert (eval y == 2)
+assert (eval sum == 1)
+assert (eval neg == -(eval sum))
+
